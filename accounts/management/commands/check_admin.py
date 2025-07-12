69af33a975361b09f27349440bc9c53286adfd5d
laith-prog <lthjawish@gmail.com>
2025-07-12 18:47:05 +0300

    modified:   accounts/management/commands/check_admin.py

--- accounts/management/commands/check_admin.py
+++ accounts/management/commands/check_admin.py
@@ -8,22 +8,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Checking admin users...'))
-        
+
         # Get all superusers
         superusers = User.objects.filter(is_superuser=True)
-        
+
         if not superusers.exists():
             self.stdout.write(self.style.WARNING('No superusers found in the database'))
             return
-            
+
         self.stdout.write(self.style.SUCCESS(f'Found {superusers.count()} superuser(s):'))
-        
+
         for user in superusers:
             self.stdout.write(self.style.SUCCESS(f'- Phone: {user.phone}'))
+            self.stdout.write(self.style.SUCCESS(f'- Phone: {user.password}'))
             self.stdout.write(self.style.SUCCESS(f'  First name: {user.first_name}'))
             self.stdout.write(self.style.SUCCESS(f'  Last name: {user.last_name}'))
             self.stdout.write(self.style.SUCCESS(f'  Email: {user.email}'))
             self.stdout.write(self.style.SUCCESS(f'  Is active: {user.is_active}'))
             self.stdout.write(self.style.SUCCESS(f'  Is staff: {user.is_staff}'))
             self.stdout.write(self.style.SUCCESS(f'  Is superuser: {user.is_superuser}'))
-            self.stdout.write(self.style.SUCCESS('---')) 
+            self.stdout.write(self.style.SUCCESS('---'))
