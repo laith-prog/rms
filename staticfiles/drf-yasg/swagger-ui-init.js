c2cb470c617038299daafb98e5b327df873b27d1
laith-prog <lthjawish@gmail.com>
2025-07-14 12:22:37 +0300

    Remove CSRF token handling from Swagger UI

--- staticfiles/drf-yasg/swagger-ui-init.js
+++ staticfiles/drf-yasg/swagger-ui-init.js
@@ -36,28 +36,9 @@ var swaggerUiConfig = {
     ],
     layout: "StandaloneLayout",
     filter: true,
-    csrfCookie: 'csrftoken',
-    csrfHeader: 'X-CSRFToken',
+    // Removed CSRF related properties since we're using JWT authentication
     requestInterceptor: function (request) {
-        var headers = request.headers || {};
-        var csrftoken = document.querySelector("[name=csrfmiddlewaretoken]");
-        if (csrftoken) {
-            csrftoken = csrftoken.value;
-        } else {
-            var cookies = document.cookie.split(/;\s+/);
-            var name = swaggerUiConfig.csrfCookie;
-            for (var i = 0; i < cookies.length; i++) {
-                if (cookies[i].indexOf(name) === 0) {
-                    csrftoken = cookies[i].slice(cookies[i].indexOf('=') + 1);
-                    break;
-                }
-            }
-        }
-
-        if (csrftoken) {
-            headers[swaggerUiConfig.csrfHeader] = csrftoken;
-        }
-
+        // Removed CSRF token handling since we're using JWT authentication
         return request;
     }
 };
